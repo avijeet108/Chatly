19368ea7d0d3455afc17864a21f3cb0729c7019d
avijeet108 <kumaravijeet108@gamil.com>
2022-02-22 00:05:34 +0530

    Fixes

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -3,7 +3,7 @@ import 'package:chat_app/screens/HomeScreen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'Registration.dart';
+import 'package:chat_app/screens/RegistrationScreen.dart';
 
 class Login extends StatefulWidget {
   @override
