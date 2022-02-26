05c5ad82ace9b4ff10e318bcc33629e6f2df0fc4
avijeet108 <kumaravijeet108@gamil.com>
2022-02-26 18:44:16 +0530

    Navigation Added

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -6,6 +6,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../group_chatting/group_chat_screen.dart';
+
 class Home extends StatefulWidget {
   @override
   State<Home> createState() => _HomeState();
@@ -74,120 +76,128 @@ class _HomeState extends State<Home> with WidgetsBindingObserver {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-        backgroundColor: Colors.lime,
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: pink,
-          title: Text(
-            "Search User",
-            style: GoogleFonts.akayaTelivigala(
-                textStyle: TextStyle(
-                    color: Colors.white,
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold)),
-          ),
-          actions: [
-            IconButton(
-                icon: Icon(
-                  Icons.logout,
+      backgroundColor: Colors.lime,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: pink,
+        title: Text(
+          "Search User",
+          style: GoogleFonts.akayaTelivigala(
+              textStyle: TextStyle(
                   color: Colors.white,
-                ),
-                onPressed: () => logOut(context))
-          ],
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold)),
         ),
-        body: isLoading == true
-            ? Center(
+        actions: [
+          IconButton(
+              icon: Icon(
+                Icons.logout,
+                color: Colors.white,
+              ),
+              onPressed: () => logOut(context))
+        ],
+      ),
+      body: isLoading == true
+          ? Center(
+              child: Container(
+                height: size.height / 20,
+                width: size.height / 20,
+                child: CircularProgressIndicator(color: purple),
+              ),
+            )
+          : Column(children: [
+              SizedBox(
+                height: size.height / 20,
+              ),
+              Container(
+                height: size.height / 14,
+                width: size.width,
+                alignment: Alignment.center,
                 child: Container(
-                  height: size.height / 20,
-                  width: size.height / 20,
-                  child: CircularProgressIndicator(color: purple),
-                ),
-              )
-            : Column(children: [
-                SizedBox(
-                  height: size.height / 20,
-                ),
-                Container(
                   height: size.height / 14,
-                  width: size.width,
-                  alignment: Alignment.center,
-                  child: Container(
-                    height: size.height / 14,
-                    width: size.width / 1.15,
-                    child: TextFormField(
-                      style: TextStyle(color: Colors.white),
-                      cursorColor: Colors.white,
-                      cursorHeight: 22.0,
-                      autofocus: false,
-                      controller: _search,
-                      decoration: InputDecoration(
-                          fillColor: pink,
-                          filled: true,
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.white,
-                                width: 2.0,
-                              ),
-                              borderRadius: BorderRadius.circular(30.0)),
-                          focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.white,
-                                width: 2.0,
-                              ),
-                              borderRadius: BorderRadius.circular(30.0)),
-                          suffixIcon: IconButton(
-                            onPressed: onSearch,
-                            icon: Icon(
-                              Icons.search,
+                  width: size.width / 1.15,
+                  child: TextFormField(
+                    style: TextStyle(color: Colors.white),
+                    cursorColor: Colors.white,
+                    cursorHeight: 22.0,
+                    autofocus: false,
+                    controller: _search,
+                    decoration: InputDecoration(
+                        fillColor: pink,
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(
                               color: Colors.white,
+                              width: 2.0,
                             ),
+                            borderRadius: BorderRadius.circular(30.0)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(30.0)),
+                        suffixIcon: IconButton(
+                          onPressed: onSearch,
+                          icon: Icon(
+                            Icons.search,
+                            color: Colors.white,
                           ),
-                          //labelText: 'Search',
-                          labelStyle: GoogleFonts.akayaTelivigala(
-                              textStyle: TextStyle(
-                                  color: Colors.white, fontSize: 22))),
-                    ),
+                        ),
+                        //labelText: 'Search',
+                        labelStyle: GoogleFonts.akayaTelivigala(
+                            textStyle:
+                                TextStyle(color: Colors.white, fontSize: 22))),
                   ),
                 ),
-                SizedBox(
-                  height: size.height / 50,
-                ),
-                SizedBox(
-                  height: size.height / 30,
-                ),
-                userMap != null
-                    ? ListTile(
-                        onTap: () {
-                          String roomId = chatRoomId(
-                              _auth.currentUser!.displayName!,
-                              userMap!['name']);
+              ),
+              SizedBox(
+                height: size.height / 50,
+              ),
+              SizedBox(
+                height: size.height / 30,
+              ),
+              userMap != null
+                  ? ListTile(
+                      onTap: () {
+                        String roomId = chatRoomId(
+                            _auth.currentUser!.displayName!, userMap!['name']);
 
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => ChatRoom(
-                                  chatRoomId: roomId,
-                                  userMap: userMap!,
-                                ),
-                              ));
-                        },
-                        leading: Icon(Icons.account_box, color: pink),
-                        title: Text(
-                          userMap!['name'],
-                          style: TextStyle(
-                            color: purple,
-                            fontSize: 17,
-                            fontWeight: FontWeight.w500,
-                          ),
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ChatRoom(
+                                chatRoomId: roomId,
+                                userMap: userMap!,
+                              ),
+                            ));
+                      },
+                      leading: Icon(Icons.account_box, color: pink),
+                      title: Text(
+                        userMap!['name'],
+                        style: TextStyle(
+                          color: purple,
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
                         ),
-                        subtitle: Text(
-                          userMap!['email'],
-                          style: TextStyle(color: pink),
-                        ),
-                        trailing: Icon(Icons.chat, color: pink),
-                      )
-                    : Container(),
-              ]));
+                      ),
+                      subtitle: Text(
+                        userMap!['email'],
+                        style: TextStyle(color: pink),
+                      ),
+                      trailing: Icon(Icons.chat, color: pink),
+                    )
+                  : Container(),
+            ]),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.group),
+        onPressed: () => Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (_) => GroupChatHomeScreen(),
+          ),
+        ),
+      ),
+    );
   }
 }
 
